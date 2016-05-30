b4ac91849f72facbc30657de9261016ac7b20b30
suits-at <sebastian.ulbel@gmail.com>
2016-05-30 16:48:53 +0200

    added elections, states route created elections.json added wien.json

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,7 +15,27 @@ $app->get('/', function () use ($app) {
     return $app->version();
 });
 
-$app->get('election', function () {
-    $states = file_get_contents("data/json/states.json");
+$app->get('/elections', function () {
+    $election = file_get_contents("data/json/elections.json");
+    return $election;
+});
+
+$app->get('/{electionId}/states', function ($electionId) {
+    $electionsData = file_get_contents("data/json/elections.json");
+    $elections = json_decode($electionsData);
+
+    $states = 'no states found';
+    foreach ($elections as $election) {
+        if ( $election->id == $electionId) {
+            $states = json_encode($election->states);
+            break;
+        }
+    }
+
     return $states;
+});
+
+$app->get('/:electionId/:stateId/districts', function () {
+    $districts = file_get_contents("data/json/states.json");
+    return $districts;
 });
