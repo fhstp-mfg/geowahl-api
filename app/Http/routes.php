3f5cab9e2ffeb0dc8197e24dd558920e7f0bb754
suits-at <sebastian.ulbel@gmail.com>
2016-05-31 11:06:59 +0200

    routes now uses deliverJson() to fix encoding problems

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,85 +12,84 @@
 */
 
 $app->get('/', function () use ($app) {
-  return $app->version();
+    return $app->version();
 });
 
-$app->get('/elections',
-  function () {
+$app->get('/elections', function () {
     $elections = getElections();
-
-    return response()->json($elections);
-  }
+    return deliverJson($elections);
+}
 );
 
-$app->get('/{electionId}/states',
-  function ($electionId) {
+$app->get('/{electionId}/states', function ($electionId) {
     $states = getStates($electionId);
-
-    return response()->json($states);
-  }
+    return deliverJson($states);
+}
 );
 
-$app->get('/{electionId}/{stateSlug}/districts',
-  function ($electionId, $stateSlug) {
+$app->get('/{electionId}/{stateSlug}/districts', function ($electionId, $stateSlug) {
     $districts = getDistricts($electionId, $stateSlug);
-
-    return response()->json($districts);
-  }
+    return deliverJson($districts);
+}
 );
-
-
-// ...
 
 /// END routes
 
-
-
-
-
 /// Elections
 
-function getElections () {
-  $electionsData = file_get_contents('data/json/elections.json');
-  $elections     = json_decode($electionsData);
+function deliverJson($data){
+    $responsecode = 200;
 
-  return $elections;
+    $header = array(
+        'Content-Type' => 'application/json; charset=UTF-8',
+        'charset' => 'utf-8'
+    );
+
+    return response()->json($data, $responsecode, $header, JSON_UNESCAPED_UNICODE);
 }
 
+function getElections()
+{
+    $electionsData = file_get_contents('data/json/elections.json');
+    $elections = json_decode($electionsData);
+
+    return $elections;
+}
 
 /// States
 
-function getStates ($electionId) {
-  $elections = getElections();
-  $states   = 'no states found';
+function getStates($electionId)
+{
+    $elections = getElections();
+    $states = 'no states found';
 
-  foreach ($elections as $election ) {
-    if ( $election->id == $electionId ) {
-      $states = $election->states;
-      break;
+    foreach ($elections as $election) {
+        if ($election->id == $electionId) {
+            $states = $election->states;
+            break;
+        }
     }
-  }
 
-  return $states;
+    return $states;
 }
-
 
 /// Districts
 
-function getDistricts ($electionId, $stateSlug) {
-  $states    = getStates($electionId);
-  $districts = 'no districts found';
+function getDistricts($electionId, $stateSlug)
+{
+    $states = getStates($electionId);
+    $districts = 'no districts found';
 
-  foreach ($states as $state) {
-    if ( $state->slug == $stateSlug ) {
-      // NOTE interpolation is nicer but slower than concatenation
-      // $districtPath  = "data/json/$stateSlug.json";
-      $districtPath  = 'data/json/'.$stateSlug.'.json';
-      $districtsData = file_get_contents($districtPath);
-      $districts     = json_decode($districtsData);
-      break;
+    foreach ($states as $state) {
+        if ($state->slug == $stateSlug) {
+            // NOTE interpolation is nicer but slower than concatenation
+            // $districtPath  = "data/json/$stateSlug.json";
+            $districtPath = 'data/json/' . $stateSlug . '.json';
+            $districtsData = file_get_contents($districtPath);
+            $districts = json_decode($districtsData);
+            break;
+        }
     }
-  }
 
-  return $districts;
+    return $districts;
 }
