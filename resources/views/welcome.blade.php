1110eb6f3b8e97803007ed57c5503d027d6dd680
Iosif Miclaus <iosif.miclaus@gmail.com>
2016-06-09 21:08:57 +0200

    Code revision welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,54 +1,54 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>GeoWahl</title>
+  <head>
+    <title>GeoWahl</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-        <!-- Place this tag in your head or just before your close body tag. -->
-        <script async defer src="https://buttons.github.io/buttons.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+    <!-- Place this tag in your head or just before your close body tag. -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
 
-        <style>
-            html, body {
-                height: 100%;
-            }
+    <style>
+      html, body {
+        height: 100%;
+      }
 
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
+      body {
+        margin: 0;
+        padding: 0;
+        width: 100%;
+        display: table;
+        font-weight: 100;
+        font-family: 'Lato';
+      }
 
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
+      .container {
+        text-align: center;
+        display: table-cell;
+        vertical-align: middle;
+      }
 
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
+      .content {
+        text-align: center;
+        display: inline-block;
+      }
 
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <a href="https://github.com/fhstp-mfg/geowahl-api"><img src="public/assets/img/Logo_round.png"/></a>
-                <div class="title">JOIN THE AWESOMENESS BRUDI</div>
-                <!-- Place this tag where you want the button to render. -->
-                <a class="github-button" href="https://github.com/fhstp-mfg/geowahl"
-                  data-icon="octicon-star" data-style="mega" data-count-href="/fhstp-mfg/geowahl/stargazers"
-                  data-count-api="/repos/fhstp-mfg/geowahl#stargazers_count" data-count-aria-label="# stargazers on GitHub"
-                  aria-label="Star fhstp-mfg/geowahl on GitHub">Star
-                </a>
-            </div>
-        </div>
-    </body>
+      .title {
+        font-size: 96px;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <div class="content">
+        <a href="https://github.com/fhstp-mfg/geowahl-api"><img src="public/assets/img/Logo_round.png"/></a>
+        <div class="title">JOIN THE AWESOMENESS BRUDI</div>
+        <!-- Place this tag where you want the button to render. -->
+        <a class="github-button" href="https://github.com/fhstp-mfg/geowahl"
+          data-icon="octicon-star" data-style="mega" data-count-href="/fhstp-mfg/geowahl/stargazers"
+          data-count-api="/repos/fhstp-mfg/geowahl#stargazers_count" data-count-aria-label="# stargazers on GitHub"
+          aria-label="Star fhstp-mfg/geowahl on GitHub">Star
+        </a>
+      </div>
+    </div>
+  </body>
 </html>
